324fb0180caa7301232632f64ee66694cafd4a13
Shubham Anand <anandshubham.emilence@gmail.com>
2019-01-29 13:04:10 +0530

    Refactor middleware

--- server.js
+++ server.js
@@ -2,21 +2,7 @@ var express = require('express');
 var app = express();
 var PORT = 2000;
 
-// app.get('/', function(req, res) {
-//     res.send('Hello Express!');
-// });
-
-var middleware = {
-    requireAuthentication: function (req, res, next) {
-        console.log('Private log hit...');
-        next();
-    },
-    logger:
-        function (req, res, next) {
-            console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
-            next();
-        }
-};
+var middleware = require('./middleware.js');
 
 app.use(middleware.logger);
 
